ef6b9caa1dbca860659929dc2c4abd299ca03bff
Mostafa Shmaisani <shmaisanimostafa@gmail.com>
2023-08-22 17:15:58 +0300

    Create Victory Screen
    - Create the UI

--- scenes/managers/arena_time_manager.gd
+++ scenes/managers/arena_time_manager.gd
@@ -1,6 +1,13 @@
 extends Node
 
+@export var victory_screen_scene: PackedScene
 @onready var timer = $Timer
+
 
 func get_time_elapsed():
 	return timer.wait_time - timer.time_left
+
+
+func _on_timer_timeout():
+	var victory_screen_instance = victory_screen_scene.instantiate()
+	add_child(victory_screen_instance)
