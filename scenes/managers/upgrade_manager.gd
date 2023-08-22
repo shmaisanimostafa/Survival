dccbe206425a229be363f04e4351855719bca177
Mostafa Shmaisani <shmaisanimostafa@gmail.com>
2023-08-22 10:54:34 +0300

    Enable Upgrade Selection

--- scenes/managers/upgrade_manager.gd
+++ scenes/managers/upgrade_manager.gd
@@ -11,13 +11,13 @@ func _on_experience_manager_level_up(_current_level: int):
 	var chosen_upgrade = upgrade_pool.pick_random() as AbilityUpgrade
 	if chosen_upgrade == null:
 		return
-	
 	var upgrade_screen_instance = upgrade_screen_scene.instantiate()
 	add_child(upgrade_screen_instance)
 	upgrade_screen_instance.set_ability_upgrades([chosen_upgrade] as Array[AbilityUpgrade])
-	
+	upgrade_screen_instance.upgrade_selected.connect(on_upgrade_selected)
+
+
 func apply_upgrade(upgrade: AbilityUpgrade):
-	
 	var has_upgrade = current_upgrades.has(upgrade.id)
 	if not has_upgrade:
 		current_upgrades[upgrade.id] = {
@@ -26,3 +26,8 @@ func apply_upgrade(upgrade: AbilityUpgrade):
 		}
 	else:
 		current_upgrades[upgrade.id]["quantity"] += 1
+	print(current_upgrades)
+
+
+func on_upgrade_selected(upgrade: AbilityUpgrade):
+	apply_upgrade(upgrade)
--- scenes/ui/ability_upgrade_card.gd
+++ scenes/ui/ability_upgrade_card.gd
@@ -1,8 +1,15 @@
 extends PanelContainer
 
+signal selected
+
 @onready var name_label: Label = $VBoxContainer/NameLabel
 @onready var description_label: Label = $VBoxContainer/DescriptionLabel
 
 func set_ability_upgrade(upgrade: AbilityUpgrade):
 	name_label.text = upgrade.name
 	description_label.text = upgrade.description
+
+
+func _on_gui_input(event: InputEvent):
+	if event.is_action_pressed("left_click"):
+		selected.emit()
