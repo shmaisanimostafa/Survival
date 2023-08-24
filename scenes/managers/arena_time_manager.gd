df5c3b444f56f6c9722a6e08306f643ab5562a77
Mostafa Shmaisani <shmaisanimostafa@gmail.com>
2023-08-24 12:12:57 +0300

    Increase Difficulty Over Time

--- scenes/managers/arena_time_manager.gd
+++ scenes/managers/arena_time_manager.gd
@@ -1,7 +1,26 @@
 extends Node
 
+
+signal arena_difficulty_increased(arena_difficulty: int)
+
+
+const DIFFICULTY_INTERVAL = 5
+
+
 @export var end_screen_scene: PackedScene
 @onready var timer = $Timer
+var arena_difficulty = 0
+
+
+func _ready():
+	pass
+
+
+func _process(_delta):
+	var next_time_target = timer.wait_time - (arena_difficulty + 1) * DIFFICULTY_INTERVAL
+	if timer.time_left <= next_time_target:
+		arena_difficulty += 1
+		arena_difficulty_increased.emit(arena_difficulty)
 
 
 func get_time_elapsed():
