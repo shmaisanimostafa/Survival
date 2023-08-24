1142aec19177fa062d8ce0df8f3c82c82eca7975
Mostafa Shmaisani <shmaisanimostafa@gmail.com>
2023-08-24 14:45:31 +0300

    Prevent Invalid Spawning
    - Prevent enemies from spawning outside the walls
    - Create a raycast

--- scenes/managers/enemy_manager.gd
+++ scenes/managers/enemy_manager.gd
@@ -22,12 +22,11 @@ func _on_timer_timeout():
 	if player == null:
 		return
 	
-	var random_direction = Vector2.RIGHT.rotated(randf_range(0, TAU))
-	var spawn_position = player.global_position + ( random_direction * SPAWN_RADIUS )
+	
 	var enemy = basic_enemy_scene.instantiate() as Node2D
 	var entities_layer = get_tree().get_first_node_in_group("entities_layer")
 	entities_layer.add_child(enemy)
-	enemy.global_position = spawn_position
+	enemy.global_position = get_spawn_position()
 	timer.start()
 
 
@@ -37,3 +36,20 @@ func on_arena_difficulty_increased(arena_difficulty: int):
 	print(time_off)
 	$Timer.wait_time = base_spawn_time - time_off
 	
+func get_spawn_position():
+	var player = get_tree().get_first_node_in_group("player") as Node2D
+	if player == null:
+		return Vector2.ZERO
+	
+	var spawn_position = Vector2.ZERO
+	for i in 4:
+		var random_direction = Vector2.RIGHT.rotated(randf_range(0, TAU))
+		spawn_position = player.global_position + ( random_direction * SPAWN_RADIUS )
+		var query_parameters = PhysicsRayQueryParameters2D.create(player.global_position, spawn_position, 1)
+		var result = get_tree().root.world_2d.direct_space_state.intersect_ray(query_parameters)
+		if result.is_empty():
+			return spawn_position
+		else:
+			random_direction = random_direction.rotated(deg_to_rad(90))
+	
+	return spawn_position
