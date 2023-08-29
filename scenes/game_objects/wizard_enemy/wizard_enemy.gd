ad81eb01e26540c04a26fcc9e076eb3a5a819537
Mostafa Shmaisani <shmaisanimostafa@gmail.com>
2023-08-29 18:39:29 +0300

    Create the Wizard Enemy
    - Create the Wizard Enemy
    - Create the Velocity Component which is attached to every enemy to make it move

--- scenes/game_objects/wizard_enemy/wizard_enemy.gd
+++ scenes/game_objects/wizard_enemy/wizard_enemy.gd
@@ -1,14 +1,14 @@
 extends CharacterBody2D
 
+@onready var health_component = $HealthComponent
+@onready var velocity_component = $VelocityComponent
 @onready var visuals = $Visuals
-@onready var velocity_component = $VelocityComponent 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta):
+
+func _process(delta):
 	velocity_component.accelerate_to_player()
 	velocity_component.move(self)
-	
 	# Make the enemy look to the moving direction ( to the player )
 	var move_sign = sign(velocity.x)
 	if move_sign != 0:
-		visuals.scale = Vector2(move_sign, 1)
+		visuals.scale = Vector2(-move_sign, 1)
