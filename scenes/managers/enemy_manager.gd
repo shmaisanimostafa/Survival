ebd2bd4fcfd00ef6adfed1f21096513eb5fa35c5
Mostafa Shmaisani <shmaisanimostafa@gmail.com>
2023-08-29 21:07:55 +0300

    Spawn Multiple Enemies
    - Create a Weighting Table for Enemy Spawning
    - Start Wizard Spawning after 30 sec

--- scenes/managers/enemy_manager.gd
+++ scenes/managers/enemy_manager.gd
@@ -3,13 +3,17 @@ extends Node
 const SPAWN_RADIUS = 330
 
 @export var basic_enemy_scene: PackedScene
+@export var wizard_enemy_scene: PackedScene
 @export var arena_time_manager: Node
 
 @onready var timer = $Timer
 
 var base_spawn_time = 0
+var enemy_table = weightedTable.new()
+
 
 func _ready():
+	enemy_table.add_item(basic_enemy_scene, 10)
 	base_spawn_time = timer.wait_time
 	arena_time_manager.arena_difficulty_increased.connect(on_arena_difficulty_increased)
 
@@ -22,8 +26,9 @@ func _on_timer_timeout():
 	if player == null:
 		return
 	
+	var enemy_scene = enemy_table.pick_item()
+	var enemy = enemy_scene.instantiate() as Node2D
 	
-	var enemy = basic_enemy_scene.instantiate() as Node2D
 	var entities_layer = get_tree().get_first_node_in_group("entities_layer")
 	entities_layer.add_child(enemy)
 	enemy.global_position = get_spawn_position()
@@ -34,8 +39,11 @@ func on_arena_difficulty_increased(arena_difficulty: int):
 	var time_off = (.1/ 12) * arena_difficulty
 	time_off = min(time_off, .7)
 	print(time_off)
-	$Timer.wait_time = base_spawn_time - time_off
-	
+	timer.wait_time = base_spawn_time - time_off
+	if arena_difficulty == 6:
+		enemy_table.add_item(wizard_enemy_scene, 20)
+
+
 func get_spawn_position():
 	var player = get_tree().get_first_node_in_group("player") as Node2D
 	if player == null:
