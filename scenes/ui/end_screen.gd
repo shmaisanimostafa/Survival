45fa867d847451294309353a44ab8785fd9ff6ff
Mostafa Shmaisani <shmaisanimostafa@gmail.com>
2023-08-30 22:00:51 +0300

    Animate the End Screens

--- scenes/ui/end_screen.gd
+++ scenes/ui/end_screen.gd
@@ -1,7 +1,16 @@
 extends CanvasLayer
 
+@onready var panel = %Panel
+
 
 func _ready():
+	
+	panel.pivot_offset = panel.size / 2
+	var tween = create_tween()
+	tween.tween_property(panel, "scale", Vector2.ZERO, 0)
+	tween.tween_property(panel, "scale", Vector2.ONE, .3)\
+	.set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_BACK)
+	
 	get_tree().paused = true
 
 
