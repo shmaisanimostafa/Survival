2ebf722e2e764f872e764bbfc205b1b03cd01720
Mostafa Shmaisani <shmaisanimostafa@gmail.com>
2023-08-31 15:40:28 +0300

    Create an Options Menu

--- scenes/ui/main_menu.gd
+++ scenes/ui/main_menu.gd
@@ -1,13 +1,18 @@
 extends CanvasLayer
 
+var options_scene = preload("res://scenes/ui/options_menu.tscn")
 
 func _on_play_button_pressed():
 	get_tree().change_scene_to_file("res://scenes/main/main.tscn")
 
 
 func _on_options_button_pressed():
-	pass # Replace with function body.
-
+	var options_instance = options_scene.instantiate()
+	add_child(options_instance)
+	options_instance.back_pressed.connect(on_options_closed.bind(options_instance))
 
 func _on_quit_button_pressed():
 	get_tree().quit()
+
+func on_options_closed(options_instance: Node):
+	options_instance.queue_free()
