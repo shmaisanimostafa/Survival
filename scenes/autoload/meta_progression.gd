9b831e72256bb0f0150ccbc915a672f1cd2356d5
Mostafa Shmaisani <shmaisanimostafa@gmail.com>
2023-09-01 12:26:03 +0300

    Save and Load Meta Progression Data

--- scenes/autoload/meta_progression.gd
+++ scenes/autoload/meta_progression.gd
@@ -3,7 +3,7 @@ extends Node
 var save_data: Dictionary = {
 #	"win_count": 0,
 #	"loss_count": 0,
-	"meta_upgrade_currency:": 0,
+	"meta_upgrade_currency": 0,
 	"meta_upgrades": {
 #		"experince_gain":{
 #			"quantity": 1,
@@ -11,12 +11,17 @@ var save_data: Dictionary = {
 	}
 }
 
+var Save_File_Path = "user://game.save"
+
+
 func _ready():
 	GameEvents.experience_vial_collected.connect(on_experience_collected)
+	load_save_file()
 
-	
+
 func on_experience_collected(number: float):
 	save_data["meta_upgrade_currency"] += number
+
 
 func add_meta_upgrade(upgrade: MetaUpgrade):
 	if not save_data["meta_upgrades"].has(upgrade.id):
@@ -24,4 +29,20 @@ func add_meta_upgrade(upgrade: MetaUpgrade):
 			"quantity": 0
 		}
 	save_data["meta_upgrades"][upgrade.id]["quantity"] += 1
+
+
+func load_save_file():
+	if not FileAccess.file_exists(Save_File_Path):
+		return
+	var file = FileAccess.open(Save_File_Path, FileAccess.READ)
+	save_data = file.get_var()
+	
+
+func save():
+	var file = FileAccess.open(Save_File_Path, FileAccess.WRITE)
+	file.store_var(save_data)
+
+
+
+
 
--- scenes/main/main.gd
+++ scenes/main/main.gd
@@ -12,6 +12,7 @@ func on_player_died():
 	var end_screen_instance = end_screen_scene.instantiate()
 	add_child(end_screen_instance)
 	end_screen_instance.set_defeat()
+	MetaProgression.save()
 
 
 func _unhandled_input(event):
